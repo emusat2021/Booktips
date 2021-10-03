4efb8d6dea4ce445cbc55a29640ac61d4e0a8f1d
Eugenia Musat <musat.eugenia@gmail.com>
2021-10-03 19:14:23 +0200

    Add custom 404 error handler

--- app.py
+++ app.py
@@ -495,6 +495,10 @@ def logout():
     session.pop("user", None)
     return redirect(url_for("get_books"))
 
-
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
